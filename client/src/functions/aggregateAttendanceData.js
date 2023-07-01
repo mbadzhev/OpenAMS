f5cd931b49fa069009b9f322dc77dd60661f4712
Momchil Badzhev <mbadzhev@gmail.com>
2023-07-01 18:41:17 +0100

    Add function to aggregate user attendance

---
+++ client/src/functions/aggregateAttendanceData.js
@@ -0,0 +1,39 @@
+import fetchUser from "./fetchUser";
+
+// Aggregates data for all events linked to a user
+async function aggregateAttendanceData(userData) {
+  let eventsTotal = 0,
+    eventsPresent = 0,
+    eventsAbsent = 0;
+
+  try {
+    userData.events.forEach((event) => {
+      event.attendance.forEach((attendance) => {
+        eventsTotal++;
+        if (attendance.present) {
+          eventsPresent++;
+        } else {
+          eventsAbsent++;
+        }
+      });
+    });
+
+    const chartData = {
+      labels: ["Present Events", "Absent Events"],
+      datasets: [
+        {
+          label: "Number of Events",
+          data: [eventsPresent, eventsAbsent],
+          backgroundColor: ["rgba(0, 255, 0, 0.5)", "rgba(255, 0, 0, 0.5)"],
+          borderColor: ["rgba(0, 255, 0, 1)", "rgba(255, 0, 0, 1)"],
+          borderWidth: 1,
+        },
+      ],
+    };
+
+    return chartData;
+  } catch (error) {
+    console.error(error);
+  }
+}
+export default aggregateAttendanceData;
